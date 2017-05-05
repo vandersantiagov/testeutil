6b61eeeb03778942c4d3d5e0cd37a5d791ef3227
rafael.carmo <rafael.carmo@b30a88d0-f4ca-4349-b1fe-e72086269161>
2017-05-05 14:44:56 +0000

    Share project "TesteUtil" into "http://forcisz1.fazenda.mg.gov.br:7070/distgts/testeutil"
    git-svn-id: http://forcisz1.fazenda.mg.gov.br:7070/distgts/testeutil/trunk@2 b30a88d0-f4ca-4349-b1fe-e72086269161

---
+++ src/main/java/util/FTPDownloadDriveBrowser.java
@@ -0,0 +1,125 @@
+package util;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.SocketException;
+import java.util.Properties;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+
+/**
+ * A program demonstrates how to dowload files from local computer to a remote
+ * FTP server using Apache Commons Net API.
+ * @author rafael.carmo
+ */
+public class FTPDownloadDriveBrowser {
+
+	public static enum BROWSER {
+		BROWSER_FIREFOX("webdriver.gecko.driver"), BROWSER_IE("webdriver.ie.driver"), BROWSER_CHROME("webdriver.chrome.driver");
+
+		private String chaveSystemProperty;
+
+		private BROWSER(String chaveSystemProperty) {
+			this.chaveSystemProperty = chaveSystemProperty;
+		}
+
+		public String getChaveSystemProperty() {
+			return chaveSystemProperty;
+		}
+
+		public void setChaveSystemProperty(String chaveSystemProperty) {
+			this.chaveSystemProperty = chaveSystemProperty;
+		}
+
+	}
+
+	protected static String CAMINHO_PROPERTIES_DRIVER_POROS = "/stor/home/siareear/ambiente/testeFuncional/driverBrowser.properties"; 
+
+	public static void obterDriver(BROWSER browser, String version) {
+
+
+		FTPClient ftpClient = new FTPClient();
+		String caminhoLocal = null;
+		Process p = null;
+		try {
+			p = Runtime.getRuntime().exec("netsh advfirewall set global StatefulFTP disable");
+			p.waitFor();
+
+			String caminhoDriver = obtemCaminhoDrive(browser, version);
+			caminhoLocal = "C:/Ambiente/DriversBrowser/" + caminhoDriver.split("/")[caminhoDriver.split("/").length - 1];
+			File downloadDrive = new File("C:/Ambiente/DriversBrowser/" + caminhoDriver.split("/")[caminhoDriver.split("/").length - 1]);
+			if (!downloadDrive.exists()) {
+				OutputStream os = new BufferedOutputStream(new FileOutputStream(downloadDrive));
+				ftpClient = connectFtp();
+				InputStream is = ftpClient.retrieveFileStream(caminhoDriver);
+				byte[] bytesArray = new byte[4096];
+				int bytesRead = -1;
+				while ((bytesRead = is.read(bytesArray)) != -1) {
+					os.write(bytesArray, 0, bytesRead);
+				}
+
+				os.close();
+				is.close();
+			}
+
+			System.setProperty(browser.getChaveSystemProperty(), caminhoLocal);
+
+
+		} catch (IOException | InterruptedException ex) {
+			System.out.println("Error: " + ex.getMessage());
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (ftpClient.isConnected()) {
+					ftpClient.logout();
+					ftpClient.disconnect();
+				}
+				if (p != null && p.isAlive()){
+					p.destroyForcibly();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+
+	}
+
+	protected static FTPClient connectFtp() throws SocketException, IOException{
+		String server = "poros.fazenda.mg.gov.br";
+		int port = 21;
+		String user = "siareear";
+		String pass = "sef2006";
+
+		FTPClient ftpClient = new FTPClient();
+		ftpClient.connect(server, port);
+		ftpClient.login(user, pass);
+		ftpClient.enterLocalPassiveMode();
+		ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+		return ftpClient;
+	}
+
+	protected static String obtemCaminhoDrive(BROWSER browser, String version) throws IOException{
+		FTPClient ftpClient = connectFtp();	
+		try {
+			Properties prop = new Properties();
+			InputStream inputStream = ftpClient.retrieveFileStream(CAMINHO_PROPERTIES_DRIVER_POROS);        
+			prop.load(inputStream);
+			return prop.getProperty(browser + "_" + version);
+		} finally {
+			if (ftpClient.isConnected()) {
+				ftpClient.disconnect();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		FTPDownloadDriveBrowser.obterDriver(BROWSER.BROWSER_IE, "11");
+	}
+
+}
---
+++ src/test/java/exemplo/SampleHeadlessTest.java
@@ -0,0 +1,64 @@
+package exemplo;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import util.FTPDownloadDriveBrowser;
+import util.FTPDownloadDriveBrowser.BROWSER;
+
+public class SampleHeadlessTest {
+	private WebDriver driver;
+
+	private String baseUrl;
+	protected static DesiredCapabilities dCaps;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		DesiredCapabilities ieCapabilities = DesiredCapabilities.internetExplorer();  
+		ieCapabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		FTPDownloadDriveBrowser.obterDriver(BROWSER.BROWSER_IE, "11");
+		FTPDownloadDriveBrowser.obterDriver(BROWSER.BROWSER_CHROME, "58");
+		FTPDownloadDriveBrowser.obterDriver(BROWSER.BROWSER_FIREFOX, "53");
+		//driver = new ChromeDriver(); 
+		driver = new InternetExplorerDriver(ieCapabilities);
+		
+		baseUrl = "http://assertselenium.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void getLinksOfAssertSelenium() throws Exception {
+		driver.get(baseUrl);
+		//Getting all the links present in the page by a HTML tag.
+		List<WebElement>  links = driver.findElements(By.tagName("a"));
+
+		//Printing the size, will print the no of links present in the page.
+		System.out.println("Total Links present is "+links.size());
+
+		//Printing the links in the page, we get through the href attribute.
+		for(int i = 0; i<links.size();i++){
+
+			System.out.println("Links are listed "+links.get(i).getAttribute("href"));
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+//		String verificationErrorString = verificationErrors.toString();
+//		if (!"&quot;&quot;".equals(verificationErrorString)) {
+//			fail(verificationErrorString);
+//		}
+	}
+
+}
