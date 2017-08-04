151d601d0946a49b6e3f0e1cb2d72d4e4cc4f056
antonio.bernardo <antonio.bernardo@b30a88d0-f4ca-4349-b1fe-e72086269161>
2017-08-04 15:35:54 +0000

    Criado o método de Copiar Colar  e leitura no arquivo txt criado.
    git-svn-id: http://forcisz1.fazenda.mg.gov.br:7070/distgts/testeutil/trunk@37 b30a88d0-f4ca-4349-b1fe-e72086269161

--- src/main/java/br/gov/mg/testeutil/util/sicaf/UtilsSicaf.java
+++ src/main/java/br/gov/mg/testeutil/util/sicaf/UtilsSicaf.java
@@ -1,8 +1,13 @@
 package br.gov.mg.testeutil.util.sicaf;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 //import java.util.Date;
+import java.io.PrintWriter;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -76,7 +81,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 		}
 		
 		/**
-		 * M�todo para exluir os arquivo no ScreencShot
+		 * Método para exluir os arquivo no ScreencShot
 		 */
 		
 		public static void deltarArquivosScreenshot (){
@@ -88,9 +93,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 		    	}
 			}
 		}
-		
 		/**
-		 * M�todo para capturar screenshot
+		 * Método para capturar screenshot
 		 * @param fileName - Nome do arquivo
 		 */
 		public static void takeScreenshot(String fileName){
@@ -103,13 +107,117 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 				e.printStackTrace();
 			}
 		}
+		/**
+		* Método para Criar o arquivo Copiar e Colocar a informação dentro do arquivo  Arquivo.txt
+		* @param fileName - Nome do arquivo
+		*/
 		
+		static By testeCopiarLer = By.className("itemOrdenacao");
+		
+		public static void escreverEmArquivoTexto(){
+		try{
+			FileWriter canal  = new FileWriter (new File("Z:\\SeleniumScreenShots\\Arquivo.txt"));
+			PrintWriter escrever = new PrintWriter(canal);
+			escrever.println(driver.findElement(testeCopiarLer).getText());
+			escrever.close();
+			}
+			catch (Exception ex){
+				ex.printStackTrace();
+			}
+		}
+		public static WebDriverWait getWait() {
+			return wait;
+		}
 		
 		/**
-		 * Pause for X milliseconds.
-		 * 
-		 * @param iTimeInMillis
-		 */
+		* Definição Técnica dos locators utilizados na página
+		* Método para Leitura de um valor em arquivo e inserir o valor no campo editável. 
+		*/		
+		static By escreverArquivo = By.id("query");
+		
+		public static void lerArquivo(String caminho) throws IOException{
+			@SuppressWarnings("unused")
+			String conteudo = ""; 
+			try{
+				BufferedReader ler = new BufferedReader(new FileReader("Z:\\SeleniumScreenShots\\Arquivo.txt"));
+				String linha = ler.readLine();
+				wait.until(ExpectedConditions.visibilityOfElementLocated(escreverArquivo));
+				driver.findElement(escreverArquivo).clear();
+				driver.findElement(escreverArquivo).sendKeys(linha);
+							
+				try{ 
+					linha = ler.readLine();
+					while (linha != null){
+						conteudo += linha+"\r\n";
+						linha =ler.readLine();
+					}
+					ler.close();
+				
+				}catch (IOException ex){
+					System.out.println("Erro: Não foi possivel ler arquivo!");
+				}
+		   } catch (FileNotFoundException ex){
+			  }
+	    }
+		/**
+		*###########################EXLUIR ARQUIVOS QUE ESTÃO NAS PASTAS###########################
+		* Método para exluir os arquivo no ScreencShot Atividades_Fiscais_QA
+		*/
+		public static void deltarArquivosScreenshotAtividades_Fiscais_QA(){
+		File pasta = new File("Z:\\SeleniumScreenShots\\Atividades_Fiscais_QA\\");    
+		File[] arquivos = pasta.listFiles();    
+		for(File arquivo : arquivos) {
+		    if(arquivo.getName().endsWith("jpeg") || arquivo.getName().endsWith("sql") || arquivo.getName().endsWith("out") || arquivo.getName().endsWith("txt")) {
+		        arquivo.delete();
+		    	}
+			}
+		}
+		/**
+		* Método para exluir os arquivo no ScreencShot DAF_QA
+		*/
+		public static void deltarArquivosScreenshotDAF_QA(){
+		File pasta = new File("Z:\\SeleniumScreenShots\\DAF_QA\\");    
+		File[] arquivos = pasta.listFiles();    
+		for(File arquivo : arquivos) {
+		    if(arquivo.getName().endsWith("jpeg") || arquivo.getName().endsWith("sql") || arquivo.getName().endsWith("out") || arquivo.getName().endsWith("txt")) {
+		        arquivo.delete();
+		    	}
+			}
+		}
+		/**
+		*###########################CRIAR OS ARQUIVOS DE IMAGENS DAS TELAS###########################
+		* Método para capturar screenshot
+		* @param fileName - Nome do arquivo
+		*/
+		public static void takeScreenshotAtividades_Fiscais_QA(String fileName){
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	//		 Date data = new Date();
+			 try {
+				FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\Atividades_Fiscais_QA\\"+fileName+".jpeg"),true);
+	//		    FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\"+fileName+" - "+data.getTime()+".jpeg"),true);
+			} catch (IOException e) {
+			e.printStackTrace();
+			}
+		}
+		/**
+		* Método para capturar screenshot
+		* @param fileName - Nome do arquivo
+		*/
+		public static void takeScreenshotDAF_QA(String fileName){
+			File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	//	    Date data = new Date();
+		    try {
+				FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\DAF_QA\\"+fileName+".jpeg"),true);
+	//		    FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\"+fileName+" - "+data.getTime()+".jpeg"),true);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		/**
+		* Pause for X milliseconds.
+		* 
+		* @param iTimeInMillis
+		*/
 		@Deprecated
 		public static void wait(final int iTimeInMillis) {
 			try {
@@ -118,5 +226,4 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 				ex.printStackTrace();
 			}
 		}
-
 }
--- src/main/java/br/gov/mg/testeutil/util/sol/UtilsSol.java
+++ src/main/java/br/gov/mg/testeutil/util/sol/UtilsSol.java
@@ -1,8 +1,13 @@
 package br.gov.mg.testeutil.util.sol;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 //import java.util.Date;
+import java.io.PrintWriter;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -30,7 +35,7 @@ public class UtilsSol {
 	}
 
 	/**
-	 * M�todo para verificar a visibilidade de um elemento utilizando o locator
+	 * Método para verificar a visibilidade de um elemento utilizando o locator
 	 * @param locator
 	 */
 	public static void isVisible(By locator) {
@@ -38,7 +43,7 @@ public class UtilsSol {
 	}
 	
 	/**
-	 * M�todo para verificar a visibilidade de um elemento utilizando o ID
+	 * Método para verificar a visibilidade de um elemento utilizando o ID
 	 * @param id
 	 */
 	public static void isVisible(String id) {
@@ -46,7 +51,7 @@ public class UtilsSol {
 	}
 	
 	/**
-	 * M�todo para verificar a presen�a de um elemento utilizando o locator
+	 * Método para verificar a presen�a de um elemento utilizando o locator
 	 * @param locator
 	 */
 	public static void isLocated(By locator) {
@@ -54,7 +59,7 @@ public class UtilsSol {
 	}
 	
 	/**
-	 * M�todo para verificar a presen�a de um elemento utilizando o ID
+	 * Método para verificar a presen�a de um elemento utilizando o ID
 	 * @param id
 	 */
 	public static void isLocated(String id) {
@@ -62,7 +67,7 @@ public class UtilsSol {
 	}
 	
 	/**
-	 * M�todo para verificar se um elemento � clic�vel utilizando o locator
+	 * Método para verificar se um elemento � clic�vel utilizando o locator
 	 * @param locator
 	 */
 	public static void isClickable(By locator) {
@@ -70,7 +75,7 @@ public class UtilsSol {
 	}
 	
 	/**
-	 * M�todo para verificar se um elemento � clic�vel utilizando o ID
+	 * Método para verificar se um elemento � clic�vel utilizando o ID
 	 * @param id
 	 */
 	public static void isClickable(String id) {
@@ -78,7 +83,7 @@ public class UtilsSol {
 	}
 	
 	/**
-	 * M�todo para exluir os arquivo no ScreencShot
+	 * Método para exluir os arquivo no ScreencShot
 	 */
 	
 	public static void deltarArquivosScreenshot (){
@@ -92,7 +97,7 @@ public class UtilsSol {
 	}
 	
 	/**
-	 * M�todo para capturar screenshot
+	 * Método para capturar screenshot
 	 * @param fileName - Nome do arquivo
 	 */
 	public static void takeScreenshot(String fileName){
@@ -105,7 +110,111 @@ public class UtilsSol {
 			e.printStackTrace();
 		}
 	}
+	/**
+	* Método para Criar o arquivo Copiar e Colocar a informação dentro do arquivo  Arquivo.txt
+	* @param fileName - Nome do arquivo
+	*/
+	static By testeCopiarLer = By.className("itemOrdenacao");
 	
+	public static void escreverEmArquivoTexto(){
+	try{
+		FileWriter canal  = new FileWriter (new File("Z:\\SeleniumScreenShots\\Arquivo.txt"));
+		PrintWriter escrever = new PrintWriter(canal);
+		escrever.println(driver.findElement(testeCopiarLer).getText());
+		escrever.close();
+		}
+		catch (Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	public static WebDriverWait getWait() {
+		return wait;
+	}
+	
+	/**
+	* Definição Técnica dos locators utilizados na página
+	* Método para Leitura de um valor em arquivo e inserir o valor no campo editável. 
+	*/		
+	static By escreverArquivo = By.id("query");
+	
+	public static void lerArquivo(String caminho) throws IOException{
+		@SuppressWarnings("unused")
+		String conteudo = ""; 
+		try{
+			BufferedReader ler = new BufferedReader(new FileReader("Z:\\SeleniumScreenShots\\Arquivo.txt"));
+			String linha = ler.readLine();
+			wait.until(ExpectedConditions.visibilityOfElementLocated(escreverArquivo));
+			driver.findElement(escreverArquivo).clear();
+			driver.findElement(escreverArquivo).sendKeys(linha);
+						
+			try{ 
+				linha = ler.readLine();
+				while (linha != null){
+					conteudo += linha+"\r\n";
+					linha =ler.readLine();
+				}
+				ler.close();
+			
+			}catch (IOException ex){
+				System.out.println("Erro: Não foi possivel ler arquivo!");
+			}
+	   } catch (FileNotFoundException ex){
+		  }
+    }
+	/**
+	*###########################EXLUIR ARQUIVOS QUE ESTÃO NAS PASTAS###########################
+	* Método para exluir os arquivo no ScreencShot Atividades_Fiscais_QA
+	*/
+	public static void deltarArquivosScreenshotAtividades_Fiscais_QA(){
+	File pasta = new File("Z:\\SeleniumScreenShots\\Atividades_Fiscais_QA\\");    
+	File[] arquivos = pasta.listFiles();    
+	for(File arquivo : arquivos) {
+	    if(arquivo.getName().endsWith("jpeg") || arquivo.getName().endsWith("sql") || arquivo.getName().endsWith("out") || arquivo.getName().endsWith("txt")) {
+	        arquivo.delete();
+	    	}
+		}
+	}
+	/**
+	* Método para exluir os arquivo no ScreencShot DAF_QA
+	*/
+	public static void deltarArquivosScreenshotDAF_QA(){
+	File pasta = new File("Z:\\SeleniumScreenShots\\DAF_QA\\");    
+	File[] arquivos = pasta.listFiles();    
+	for(File arquivo : arquivos) {
+	    if(arquivo.getName().endsWith("jpeg") || arquivo.getName().endsWith("sql") || arquivo.getName().endsWith("out") || arquivo.getName().endsWith("txt")) {
+	        arquivo.delete();
+	    	}
+		}
+	}
+	/**
+	*###########################CRIAR OS ARQUIVOS DE IMAGENS DAS TELAS###########################
+	* Método para capturar screenshot
+	* @param fileName - Nome do arquivo
+	*/
+	public static void takeScreenshotAtividades_Fiscais_QA(String fileName){
+	File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//		 Date data = new Date();
+		 try {
+			FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\Atividades_Fiscais_QA\\"+fileName+".jpeg"),true);
+//		    FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\"+fileName+" - "+data.getTime()+".jpeg"),true);
+		} catch (IOException e) {
+		e.printStackTrace();
+		}
+	}
+	/**
+	* Método para capturar screenshot
+	* @param fileName - Nome do arquivo
+	*/
+	public static void takeScreenshotDAF_QA(String fileName){
+		File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+//	    Date data = new Date();
+	    try {
+			FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\DAF_QA\\"+fileName+".jpeg"),true);
+//		    FileUtils.copyFile(scrFile, new File("Z:\\SeleniumScreenShots\\"+fileName+" - "+data.getTime()+".jpeg"),true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	/**
 	 * Pause for X milliseconds.
