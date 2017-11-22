5266d8174ed82a9d8865c29545dcf74b27696f7d
sandra.rodrigues <sandra.rodrigues@b30a88d0-f4ca-4349-b1fe-e72086269161>
2017-11-22 19:11:56 +0000

    Correções na geração do relatório: -- Alteração na estrutura de pastas do report; -- Correção no maneira de criar diretorios; -- Inclusão da possibilidade de fechar ou não o ambiente ao finalizar os testes. -- Correção de print de erros no console, de System.out.println para e.printStackTrace.
    git-svn-id: http://forcisz1.fazenda.mg.gov.br:7070/distgts/testeutil/trunk@123 b30a88d0-f4ca-4349-b1fe-e72086269161

--- src/main/java/br/gov/mg/testeutil/report/ST0000_ReportSuiteProjeto.java
+++ src/main/java/br/gov/mg/testeutil/report/ST0000_ReportSuiteProjeto.java
@@ -1,18 +1,44 @@
 package br.gov.mg.testeutil.report;
 
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.AfterClass;
 import org.junit.ClassRule;
 
 import br.gov.mg.testeutil.report.rules.RuleReport;
 import br.gov.mg.testeutil.report.rules.RuleReportSuiteProjeto;
+import br.gov.mg.testeutil.report.rules.SuiteSiare;
 
 public class ST0000_ReportSuiteProjeto {
+	private static String nomeSuite;
+	public static boolean quitAmbiente = true;
 
 	@ClassRule
 	public static RuleReport getResource() {
+		SuiteSiare.idSuite += 1;
+		RuleReportSuiteProjeto.isSuiteTotal = false;
 		return RuleReportSuiteProjeto.startTestesSuiteFilha();
 	}
 
 	public static void setNomeProjeto(String nomeProjeto_, String nomeSuite_) {
+		if (StringUtils.isBlank(SuiteSiare.nomePrimeiraSuite)) {
+			RuleReportSuiteProjeto.isSuiteTotal = true;
+			SuiteSiare.nomePrimeiraSuite = nomeSuite_;
+			SuiteSiare.suitePrincipalVO.setNomeSuite(nomeSuite_);
+			nomeSuite = nomeSuite_;
+		} else {
+			RuleReportSuiteProjeto.isSuiteTotal = false;
+		}
+		SuiteSiare.quitAmbiente = quitAmbiente;
 		RuleReportSuiteProjeto.beforeClassSuite(nomeProjeto_, nomeSuite_);
+	}
+
+	@AfterClass
+	public static void afterClass() throws Exception {
+		SuiteSiare.idSuite -= 1;
+		if (Objects.equals(nomeSuite, SuiteSiare.suitePrincipalVO.getNomeSuite())) {
+			RuleReportSuiteProjeto.isSuiteTotal = true;
+		}
 	}
 }
--- src/main/java/br/gov/mg/testeutil/report/ST0000_ReportSuiteSiareTesteGeral.java
+++ src/main/java/br/gov/mg/testeutil/report/ST0000_ReportSuiteSiareTesteGeral.java
@@ -3,17 +3,27 @@ package br.gov.mg.testeutil.report;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
+import br.gov.mg.testeutil.report.rules.RuleReportSuiteProjeto;
 import br.gov.mg.testeutil.report.rules.SuiteSiare;
 
 public class ST0000_ReportSuiteSiareTesteGeral {
+	public static boolean quitAmbiente = true;
 
 	@BeforeClass
 	public static void beforeClass() {
+		RuleReportSuiteProjeto.isSuiteTotal = true;
 		SuiteSiare.startReport();
+	}
+
+	public static void setNomeSuite(String nomeSuite_) {
+		SuiteSiare.quitAmbiente = quitAmbiente;
+		SuiteSiare.nomePrimeiraSuite = nomeSuite_;
 	}
 
 	@AfterClass
 	public static void afterClass() throws Exception {
+		RuleReportSuiteProjeto.isSuiteTotal = true;
 		SuiteSiare.finalizeReport();
 	}
+
 }
